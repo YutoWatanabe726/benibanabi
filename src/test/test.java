163f21e68c2d78fb6df43a83872c5da90c83d30f
Todakoma <ymg2465017@stu.o-hara.ac.jp>
2025-11-10 14:17:17 +0900

    データベース

--- src/test/test.java
+++ src/test/test.java
@@ -1,9 +1,39 @@
 package test;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class test {
+    public static void main(String[] args) {
+        try {
+            // コンテキストから DataSource を取得
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            DataSource ds = (DataSource) envCtx.lookup("jdbc/benibanabi");
 
-	public static void main(String[] args) {
-		System.out.println("haro");
-	}
+            try (Connection conn = ds.getConnection();
+                 Statement stmt = conn.createStatement()) {
 
+                String sql = "SELECT ID, NAME FROM TEST_TABLE";
+                ResultSet rs = stmt.executeQuery(sql);
+
+                System.out.println("=== TEST_TABLEの中身 ===");
+                while (rs.next()) {
+                    int id = rs.getInt("ID");
+                    String name = rs.getString("NAME");
+                    System.out.println("ID: " + id + ", NAME: " + name);
+                }
+            }
+
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
