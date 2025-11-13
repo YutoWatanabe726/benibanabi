c40d0c1fe81e4a21ceae1be44cc6b1a67b8b7ca4
Todakoma <ymg2465017@stu.o-hara.ac.jp>
2025-11-13 09:49:57 +0900

    index.java作成11/13

--- src/benibanabi/index.java
+++ src/benibanabi/index.java
@@ -1,10 +1,34 @@
 package benibanabi;
 
-public class index {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+import tool.Action;
 
+public class index extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		//ローカル変数の宣言 1
+		//なし
+
+		//リクエストパラメータ―の取得 2
+		//なし
+
+		//DBからデータ取得 3
+		//なし
+
+		//ビジネスロジック 4
+		//なし
+
+		//DBへデータ保存 5
+		//なし
+
+		//レスポンス値をセット 6
+		//なし
+
+		//JSPへフォワード 7
+		req.getRequestDispatcher("index.jsp").forward(req, res);
 	}
-
 }
